691f062d89fddc1d9032ae12cebb1626209577cd
Jamie Kennea <jak51@psu.edu>
2025-12-09 08:33:45 -0500

    fix: speed up moon phase

--- src/constraints/moon_phase.rs
+++ src/constraints/moon_phase.rs
@@ -127,11 +127,17 @@ impl ConstraintEvaluator for MoonPhaseEvaluator {
     ) -> PyResult<ConstraintResult> {
         // Extract and filter time data
         let (times_filtered,) = extract_time_data!(ephemeris, time_indices);
+
+        // Pre-compute all illumination values to avoid redundant ephemeris calculations
+        let illuminations: Vec<f64> = (0..times_filtered.len())
+            .map(|i| calculate_moon_illumination(ephemeris, i))
+            .collect();
+
         let violations = track_violations(
             &times_filtered,
             |i| {
                 let time = &times_filtered[i];
-                let illumination = calculate_moon_illumination(ephemeris, i);
+                let illumination = illuminations[i];
                 let moon_altitude = self.calculate_moon_altitude(time);
                 let moon_distance = self.calculate_moon_distance(time, target_ra, target_dec);
 
@@ -179,7 +185,7 @@ impl ConstraintEvaluator for MoonPhaseEvaluator {
                 }
 
                 let time = &times_filtered[i];
-                let illumination = calculate_moon_illumination(ephemeris, i);
+                let illumination = illuminations[i];
                 let moon_altitude = self.calculate_moon_altitude(time);
                 let moon_distance = self.calculate_moon_distance(time, target_ra, target_dec);
                 let phase_name = self.get_moon_phase_name(illumination);
